808a45d45ecd70cf9639381dded7285e5c3007b7
PinxuanHuang <hatedog77777@gmail.com>
2023-04-17 19:31:03 +0800

    Modify the packet parsing procedure
    - parsing sk_buff with packet header offset

--- http_sniffer.c
+++ http_sniffer.c
@@ -9,42 +9,110 @@
 #include <linux/udp.h>
 #include <linux/string.h>
 
+#define MACADDRESS(addr) \
+    ((unsigned char *)addr)[0], \
+    ((unsigned char *)addr)[1], \
+    ((unsigned char *)addr)[2], \
+    ((unsigned char *)addr)[3], \
+    ((unsigned char *)addr)[4], \
+    ((unsigned char *)addr)[5]
+
+#define H_PROTO(addr) \
+    ((unsigned char *)addr)[0], \
+    ((unsigned char *)addr)[1]
+    
+
+//#define IPADDRESS(addr) \
+//	((unsigned char *)&addr)[3], \
+//	((unsigned char *)&addr)[2], \
+//	((unsigned char *)&addr)[1], \
+//	((unsigned char *)&addr)[0]
+
 #define IPADDRESS(addr) \
-	((unsigned char *)&addr)[3], \
-	((unsigned char *)&addr)[2], \
-	((unsigned char *)&addr)[1], \
-	((unsigned char *)&addr)[0]
+	((unsigned char *)addr)[3], \
+	((unsigned char *)addr)[2], \
+	((unsigned char *)addr)[1], \
+	((unsigned char *)addr)[0]
 
 static struct nf_hook_ops *http_sniffer_ops = NULL;
 
-static unsigned int is_ipv4(struct ethhdr *ethh){
-    if(ntohs(ethh->h_proto) == ETH_P_IP){
-        return 1;
-    }
-    return 0;
-}
-
-static unsigned int is_tcp(struct iphdr *iph){
-    if(iph->protocol == IPPROTO_TCP){
-        return 1;
-    }
-    return 0;
-}
+// static unsigned int is_ipv4(struct ethhdr *ethh){
+//     if(ntohs(ethh->h_proto) == ETH_P_IP){
+//         return 1;
+//     }
+//     return 0;
+// }
+// 
+// static unsigned int is_tcp(struct iphdr *iph){
+//     if(iph->protocol == IPPROTO_TCP){
+//         return 1;
+//     }
+//     return 0;
+// }
 
 static unsigned int http_sniffer(void *priv, struct sk_buff *skb, const struct nf_hook_state *state){
-    struct ethhdr *ethh;
-    ethh = eth_hdr(skb);
-    if(is_ipv4(ethh)){
-        struct iphdr *iph;
-        __u32 sip;
-        __u32 dip;
-        iph = ip_hdr(skb);
-        sip = ntohl(iph->saddr);
-        dip = ntohl(iph->daddr);
-        if(is_tcp(iph)){
-            printk(KERN_INFO "saddr:%u.%u.%u.%u daddr:%u.%u.%u.%u is tcp", IPADDRESS(sip), IPADDRESS(dip));
+    // struct ethhdr *eth_h;
+    struct iphdr *ipv4_hs;
+    // struct tcphdr *tcp_h;
+    unsigned char *eth_h;
+    unsigned char *ipv4_h;
+    unsigned char *tcp_h;
+    __be16 hproto;
+    __u8 ipproto;
+    __be32 saddr;
+    __be32 daddr;
+    u32 sip;
+    u32 dip;
+    unsigned char *http_port = "\x00\x50";
+    unsigned short *tcp_sp;
+    eth_h = skb_mac_header(skb);
+    printk(KERN_INFO "%ld", skb->len - skb->data_len);
+    if((unsigned char *)skb->tail - eth_h > 14){
+        hproto = *(eth_h + 12);
+        printk(KERN_INFO "mac header to tail : %d", skb->tail - (sk_buff_data_t)eth_h);
+        printk(KERN_INFO "========== packet ETH header ==========");
+        printk(KERN_INFO "SRC MAC ADDR -> %02x:%02x:%02x:%02x:%02x:%02x", MACADDRESS(eth_h+6));
+        printk(KERN_INFO "DES MAC ADDR -> %02x:%02x:%02x:%02x:%02x:%02x", MACADDRESS(eth_h));
+        printk(KERN_INFO "H_PROTO -> %x", ntohs(hproto));
+        printk(KERN_INFO "===================================");
+        if(ntohs(hproto) == ETH_P_IP){
+            ipv4_h = skb_network_header(skb);
+            if((unsigned char *)skb->tail - ipv4_h > 20){
+                ipv4_hs = ip_hdr(skb);
+                ipproto = *(ipv4_h + 9);
+                saddr = *(ipv4_h+12);
+                daddr = *(ipv4_h+16);
+                sip = ntohl(*(ipv4_h+12));
+                dip = ntohl(*(ipv4_h+16));
+                sip = ntohl(ipv4_hs->saddr);
+                dip = ntohl(ipv4_hs->daddr);
+                printk(KERN_INFO "ipv4 header to tail : %d", (unsigned char *)skb->tail - ipv4_h);
+                printk(KERN_INFO "========== packet IP header ==========");
+                //printk(KERN_INFO "SRC MAC ADDR -> %u.%u.%u.%u", IPADDRESS(sip));
+                //printk(KERN_INFO "DES MAC ADDR -> %u.%u.%u.%u", IPADDRESS(dip));
+                //printk(KERN_INFO "SRC MAC ADDR -> %u.%u.%u.%u", IPADDRESS(ipv4_h+12));
+                //printk(KERN_INFO "DES MAC ADDR -> %u.%u.%u.%u", IPADDRESS(ipv4_h+16));
+                printk(KERN_INFO "SRC MAC ADDR -> %pI4", ipv4_h+12);
+                printk(KERN_INFO "DES MAC ADDR -> %pI4", ipv4_h+16);
+                printk(KERN_INFO "IPPROTO -> %x", ipproto);
+                printk(KERN_INFO "==================================");
+                if(ipproto == IPPROTO_TCP){
+                    tcp_h = skb_transport_header(skb);
+                    tcp_sp = ntohs((unsigned short *)tcp_h);
+                    if((unsigned char *)skb->tail - tcp_h > 20){
+                        printk("%hu", *tcp_sp);
+                        if(*tcp_sp == 80){
+                            printk(KERN_INFO "It's http protocol");
+                        }
+                        //printk(KERN_INFO "transport header to tail : %d", (unsigned char *)skb->tail - tcp_h);
+                        printk(KERN_INFO "========== packet TCP header ==========");
+                        printk(KERN_INFO "tcp options data 5 bytes : %u %u %u %u %u", *(tcp_h+20), *(tcp_h+21), *(tcp_h+22), *(tcp_h+23), *(tcp_h+24));
+                        printk(KERN_INFO "===================================");
+                    }
+                }
+            }
         }
-    }                                        
+    }
     return NF_ACCEPT;
 }
 static int __init packet_sniffer_init(void) {
@@ -53,8 +121,7 @@ static int __init packet_sniffer_init(void) {
 		http_sniffer_ops->hook = (nf_hookfn*)http_sniffer;                           
 		http_sniffer_ops->hooknum = NF_INET_FORWARD;                                        
 		http_sniffer_ops->pf = NFPROTO_IPV4;
-		http_sniffer_ops->priority = NF_IP_PRI_FIRST;                                           
-		
+		http_sniffer_ops->priority = NF_IP_PRI_FIRST;
 		nf_register_net_hook(&init_net, http_sniffer_ops);
         printk(KERN_INFO "Init");
 	}
