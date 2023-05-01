f23148e78dfe120fcf11dc51b936203d46bbcf7b
PinxuanHuang <hatedog77777@gmail.com>
2023-05-01 14:47:27 +0800

    Refactor the http_sniffer function and add ioctl basic struct

--- http_sniffer.c
+++ http_sniffer.c
@@ -9,24 +9,43 @@
 #include <linux/udp.h>
 #include <linux/string.h>
 #include <linux/uaccess.h>
+#include <linux/fs.h>
+#include <linux/miscdevice.h>
 
-#define MACADDRESS(addr)            \
-    ((unsigned char *)addr)[0],     \
-        ((unsigned char *)addr)[1], \
-        ((unsigned char *)addr)[2], \
-        ((unsigned char *)addr)[3], \
-        ((unsigned char *)addr)[4], \
-        ((unsigned char *)addr)[5]
+#include "self_define.h"
 
-#define H_PROTO(addr)           \
-    ((unsigned char *)addr)[0], \
-        ((unsigned char *)addr)[1]
+const unsigned short HTTP_PORT = 80;
 
-#define IPADDRESS(addr)             \
-    ((unsigned char *)addr)[3],     \
-        ((unsigned char *)addr)[2], \
-        ((unsigned char *)addr)[1], \
-        ((unsigned char *)addr)[0]
+int file_open(struct inode *i, struct file *f)
+{
+    printk(KERN_INFO "[*] open dev file");
+    return 0;
+}
+
+long my_ioctl(struct file *f, unsigned int cmd, unsigned long arg)
+{
+    printk(KERN_INFO "[*] ioctl comm...");
+    return 0;
+}
+
+int file_close(struct inode *i, struct file *f)
+{
+    printk(KERN_INFO "[*] close dev file");
+    return 0;
+}
+
+struct file_operations my_fops = {
+    .open = file_open,
+    .unlocked_ioctl = my_ioctl,
+    .release = file_close,
+};
+
+struct miscdevice my_dev = {
+    .minor = MISC_DYNAMIC_MINOR,
+    .name = DEV_NAME,
+    .fops = &my_fops,
+    .mode = 0666,
+};
 
 static struct nf_hook_ops *http_sniffer_ops = NULL;
 
@@ -36,75 +55,58 @@ static unsigned int http_sniffer(void *priv, struct sk_buff *skb, const struct n
     unsigned char *ipv4_h;
     unsigned char *tcp_h;
     unsigned char *pkt_tail;
-    unsigned short http_port = 80;
-    unsigned short *tcp_sp;
-    unsigned short *tcp_dp;
-    unsigned char *data_offset;
-    unsigned char *payload;
+    unsigned short tcp_sp;
+    unsigned short tcp_dp;
     __be16 hproto;
     __u8 ipproto;
-    unsigned int payload_len;
 
     pkt_tail = skb_tail_pointer(skb);
     eth_h = skb_mac_header(skb);
-    if (pkt_tail - eth_h > 14)
+
+    if (pkt_tail - eth_h < 14)
     {
-        hproto = *(eth_h + 12);
-        // printk(KERN_INFO "mac header to tail : %ld", pkt_tail - eth_h);
-        printk(KERN_INFO "========== packet ETH header ==========");
-        printk(KERN_INFO "SRC MAC ADDR -> %02x:%02x:%02x:%02x:%02x:%02x", MACADDRESS(eth_h + 6));
-        printk(KERN_INFO "DES MAC ADDR -> %02x:%02x:%02x:%02x:%02x:%02x", MACADDRESS(eth_h));
-        // printk(KERN_INFO "H_PROTO -> %x", ntohs(hproto));
-        printk(KERN_INFO "===================================");
-        if (ntohs(hproto) == ETH_P_IP)
-        {
-            ipv4_h = skb_network_header(skb);
-            if (pkt_tail - ipv4_h > 20)
-            {
-                ipproto = *(ipv4_h + 9);
-                // printk(KERN_INFO "ipv4 header to tail : %ld", pkt_tail - ipv4_h);
-                printk(KERN_INFO "========== packet IP header ==========");
-                printk(KERN_INFO "SRC MAC ADDR -> %pI4", ipv4_h + 12);
-                printk(KERN_INFO "DES MAC ADDR -> %pI4", ipv4_h + 16);
-                // printk(KERN_INFO "IPPROTO -> %x", ipproto);
-                printk(KERN_INFO "==================================");
-                if (ipproto == IPPROTO_TCP)
-                {
-                    tcp_h = skb_transport_header(skb);
-                    tcp_sp = (unsigned short *)tcp_h;
-                    tcp_dp = (unsigned short *)(tcp_h + 2);
-                    if (pkt_tail - tcp_h > 20)
-                    {
-                        data_offset = (tcp_h + 12);
-                        printk(KERN_INFO "========== packet TCP header ==========");
-                        printk("tail to tcp header -> %ld", pkt_tail - tcp_h);
-                        printk("SRC PORT %hu", ntohs(*tcp_sp));
-                        printk("DST PORT %hu", ntohs(*tcp_dp));
-                        printk("offset = %u", (*data_offset) >> 4);
-                        printk(KERN_INFO "===================================");
-                        payload = tcp_h + ((*data_offset) >> 4) * 4; // get the tcp packet payload
-                        if (ntohs(*tcp_sp) == http_port || ntohs(*tcp_dp) == http_port)
-                        {
-                            int i;
-                            printk(KERN_INFO "====== HTTP context =======");
-                            payload_len = pkt_tail - payload;
-                            printk(KERN_INFO "payload size = %d", payload_len);
-                            for (i = 0; i < payload_len; i += 5)
-                            {
-                                if (i > 20)
-                                    break;
-                                printk(KERN_INFO "%c %c %c %c %c", *(payload + i), *(payload + i + 1), *(payload + i + 2), *(payload + i + 3), *(payload + i + 4));
-                            }
-                            printk(KERN_INFO "===========================");
-                        }
-                    }
-                }
-            }
-        }
+        return NF_ACCEPT;
     }
+
+    hproto = *(eth_h + 12);
+    if (ntohs(hproto) != ETH_P_IP)
+    {
+        return NF_ACCEPT;
+    }
+
+    ipv4_h = skb_network_header(skb);
+    if (pkt_tail - ipv4_h <= 20)
+    {
+        return NF_ACCEPT;
+    }
+
+    ipproto = *(ipv4_h + 9);
+    if (ipproto != IPPROTO_TCP)
+    {
+        return NF_ACCEPT;
+    }
+
+    tcp_h = skb_transport_header(skb);
+    if (pkt_tail - tcp_h < 20)
+    {
+        return NF_ACCEPT;
+    }
+
+    tcp_sp = ntohs(*(unsigned short *)tcp_h);
+    tcp_dp = ntohs(*(unsigned short *)(tcp_h + 2));
+    if (tcp_sp != HTTP_PORT && tcp_dp != HTTP_PORT)
+    {
+        return NF_ACCEPT;
+    }
+    printk(KERN_INFO "[*] It's http packet");
+
     return NF_ACCEPT;
 }
-static int __init packet_sniffer_init(void)
+
+/*
+ * Define functions needs in init progress
+ */
+static int hook_reg(void)
 {
     http_sniffer_ops = (struct nf_hook_ops *)kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
     if (http_sniffer_ops != NULL)
@@ -113,20 +115,64 @@ static int __init packet_sniffer_init(void)
         http_sniffer_ops->hooknum = NF_INET_FORWARD;
         http_sniffer_ops->pf = NFPROTO_IPV4;
         http_sniffer_ops->priority = NF_IP_PRI_FIRST;
-        nf_register_net_hook(&init_net, http_sniffer_ops);
-        printk(KERN_INFO "Init");
+        if (nf_register_net_hook(&init_net, http_sniffer_ops) < 0)
+        {
+            return -ENODEV;
+        }
+        printk(KERN_INFO "=== Init ===");
+    }
+    else
+    {
+        return -ENODEV;
     }
     return 0;
 }
 
-static void __exit packet_sniffer_exit(void)
+/*
+ * Define functions need in exit progress
+ */
+static void hook_unreg(void)
 {
     if (http_sniffer_ops != NULL)
     {
         nf_unregister_net_hook(&init_net, http_sniffer_ops);
         kfree(http_sniffer_ops);
     }
-    printk(KERN_INFO "Exit");
+    printk(KERN_INFO "=== Exit ===");
+}
+
+static int __init packet_sniffer_init(void)
+{
+    int fail = 0;
+
+    // register hook
+    fail = hook_reg();
+    if (fail)
+    {
+        goto HOOK_REG_FAIL;
+    }
+
+    // register device
+    fail = misc_register(&my_dev);
+    if (fail)
+    {
+        goto DEV_REG_FAIL;
+    }
+
+    return 0;
+
+DEV_REG_FAIL:
+    nf_unregister_net_hook(&init_net, http_sniffer_ops);
+    kfree(http_sniffer_ops);
+
+HOOK_REG_FAIL:
+    return -ENODEV;
+}
+
+static void __exit packet_sniffer_exit(void)
+{
+    hook_unreg();
+    misc_deregister(&my_dev);
 }
 
 module_init(packet_sniffer_init);
