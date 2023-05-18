e10ff41fdab250967d9047fc973c412fb3f98b35
PinxuanHuang <hatedog77777@gmail.com>
2023-05-18 08:41:40 +0800

    Modify struct
    - delete ft struct. It's only for practice
    - add kv, flow_data, packet_data struct

--- self_define.h
+++ self_define.h
@@ -1,16 +1,44 @@
-#ifndef _MY_SIMPLE_STRUCT_
-
-#define _MY_SIMPLE_STRUCT_
+#ifndef _SELF_DEFINE_H_
+#define _SELF_DEFINE_H_
 
 #define DEV_NAME "simple_dev"
+#define MAP_SIZE 10
 
-// five tuple
-struct ft
+// packet data
+struct packet_data
 {
-    // unsigned char sip[4];
-    // unsigned char dip[4];
-    unsigned char pkt_data[2048];
+    unsigned char payload[2048];
+    struct packet_data *next;
 };
+
+// flow data
+struct flow_data
+{
+    unsigned char data[16];
+    struct packet_data *next;
+};
+
+// flow management key value pair
+struct kv
+{
+    unsigned char key[16];
+    struct flow_data *val;
+    struct kv *next;
+};
+
+// hmap
+struct hmap
+{
+    unsigned int size;
+    struct kv **map;
+};
+
+// flow management map api interface
+struct hmap *map_init(unsigned int);
+int set_map(struct hmap *, char *, struct flow_data *);
+struct flow_data *get_map(struct hmap *, char *);
+void del_map(struct hmap *, char *);
+void clean_up(struct hmap *);
 
 enum ops
 {
