670545721e39a8a97cc376a7a2994fa393867dd2
PinxuanHuang <hatedog77777@gmail.com>
2023-05-20 18:31:37 +0800

    Modify the structure in self_define.h and functions in map_func.c
    - can compile these file together and test in user space

--- map_func.c
+++ map_func.c
@@ -2,18 +2,20 @@
 
 #ifdef __KERNEL__
 #include <linux/slab.h>
+#include <linux/string.h>
 #else
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #endif
 
-void *my_malloc(unsigned int size)
+void *my_malloc(size_t size)
 {
     void *result;
 
 #ifdef __KERNEL__
-    result = kmalloc(GFP_KERNEL, size); // GFP_KERNEL, GFP_ATOMIC
-#else                                   /* user space */
+    result = kmalloc(size, GFP_KERNEL); // GFP_KERNEL, GFP_ATOMIC
+#else /* user space */
     result = malloc(size);
 #endif
 
@@ -29,12 +31,14 @@ void my_free(void *ptr)
 #endif
 }
 
-struct hmap *map_init(unsigned int size)
-{
+/*
+Initialize the hmap
+*/
+struct hmap *map_init(unsigned int size){
     // [struct ft_kv *, ..., ...]
-    struct hmap *map = malloc(sizeof(struct hmap));
+    struct hmap *map = my_malloc(sizeof(struct hmap));
     map->size = size;
-    map->map = malloc(sizeof(struct kv *) * size);
+    map->map = my_malloc(sizeof(struct kv *) * size);
     memset(map->map, 0, sizeof(struct kv *) * size);
     return map;
 }
@@ -42,48 +46,42 @@ struct hmap *map_init(unsigned int size)
 /*
 set a key:val in the hmap
 */
-int set_map(struct hmap *map, char *key, struct flow_data *val)
-{
+int set_map(struct hmap *map, char *key, struct packet_data val){
     int exist = 0;
     int key_l = strlen(key);
-    struct kv *flow_key = map->map[key_l % MAP_SIZE];
+    struct kv *flow_key = map->map[key_l%MAP_SIZE];
     struct kv *head = flow_key;
-
+    
     // if the bucket doesn't have any flow
-    if (flow_key == NULL)
-    {
-        struct kv *new_flow = malloc(sizeof(struct kv));
+    if(flow_key == NULL){
+        struct kv *new_flow = my_malloc(sizeof(struct kv));
         strncpy(new_flow->key, key, 15);
         new_flow->key[15] = '\0';
         new_flow->val = val;
         new_flow->next = NULL;
-        map->map[key_l % MAP_SIZE] = new_flow;
+        map->map[key_l%MAP_SIZE] = new_flow;
         return 0;
     }
     // check whether the key has already exised or not
-    while (flow_key)
-    {
-        if (strcmp(flow_key->key, key) == 0)
-        {
+    while(flow_key){
+        if(strcmp(flow_key->key, key) == 0){
             exist = 1;
             break;
         }
         flow_key = flow_key->next;
     }
     // if so, update it's value
-    if (exist)
-    {
+    if(exist){
         flow_key->val = val;
     }
     // else set a new kv and update the bucket head
-    else
-    {
-        struct kv *new_flow = malloc(sizeof(struct kv));
+    else{
+        struct kv *new_flow = my_malloc(sizeof(struct kv));
         strncpy(new_flow->key, key, 15);
         new_flow->key[15] = '\0';
         new_flow->val = val;
         new_flow->next = head;
-        map->map[key_l % MAP_SIZE] = new_flow;
+        map->map[key_l%MAP_SIZE] = new_flow;
     }
     return 0;
 }
@@ -91,17 +89,14 @@ int set_map(struct hmap *map, char *key, struct flow_data *val)
 /*
 Get a value of the specific key
 */
-struct flow_data *get_map(struct hmap *map, char *key)
-{
+struct packet_data get_map(struct hmap *map, char *key){
     int key_l = strlen(key);
-    struct kv *flow_key = map->map[key_l % MAP_SIZE];
-    struct flow_data *val = NULL;
+    struct kv *flow_key = map->map[key_l%MAP_SIZE];
+    struct packet_data val = {};
 
     // if the bucket doesn't have any flow
-    while (flow_key)
-    {
-        if (strcmp(flow_key->key, key) == 0)
-        {
+    while(flow_key){
+        if(strcmp(flow_key->key, key) == 0){
             val = flow_key->val;
             break;
         }
@@ -111,37 +106,65 @@ struct flow_data *get_map(struct hmap *map, char *key)
     return val;
 }
 
-void del_map(struct hmap *map, char *key)
-{
+/*
+Delete a key from hmap
+*/
+void del_map(struct hmap *map, char *key){
     int key_l = strlen(key);
-    struct kv **indirect = &map->map[key_l % MAP_SIZE];
-    struct kv *node;
-    while (*indirect)
-    {
-        if (strcmp((*indirect)->key, key) == 0)
-        {
-            node = *indirect;
-            *indirect = node->next;
-            free(node);
-            break;
-        }
-        indirect = &(*indirect)->next;
-    }
+    struct kv **indirect = &map->map[key_l%MAP_SIZE];
+	struct kv *node;
+	while (*indirect){
+		if (strcmp((*indirect)->key, key) == 0){
+            printf("del %s val: %s\n", (*indirect)->key, (*indirect)->val.payload);
+			node = *indirect;
+			*indirect = node->next;
+			my_free(node);
+			break;
+		}
+		indirect = &(*indirect)->next;
+	}    
     return;
 }
 
-void clean_up(struct hmap *map)
-{
+/*
+Clean up the hmap
+*/
+void clean_up(struct hmap *map){
+    struct kv **indirect;
+    struct kv *node;
+    
+    for(int i=0;i<MAP_SIZE;i++){
+        indirect = &map->map[i];
+        while (*indirect){
+            printf("clean up %s val: %s\n", (*indirect)->key, (*indirect)->val.payload);
+            node = *indirect; 
+            *indirect = node->next;
+            free(node);
+        }    
+    }
     free(map->map);
     free(map);
     return;
 }
 
-int main(void)
-{
-    printf("=== Start map testing ===");
+int main(void){
+    printf("=== Start map testing ===\n");
     struct hmap *map = map_init(MAP_SIZE);
+    char *key1 = "gary";
+    char *key2 = "steve";
+    char *key3 = "leon";
+    char *key4 = "robin";
+    struct packet_data data1 = {{'h', 'e', 'l', 'l','o', '\0'}};
+    struct packet_data data2 = {{'w', 'o', 'r', 'l','d', '\0'}};
+    struct packet_data data3 = {{'a', 'b', 'c', 'd','e', '\0'}};
+    struct packet_data data4 = {{'1', '2', '3', '4', 0x97, '\0'}};
+    set_map(map, key1, data1);
+    set_map(map, key2, data2);
+    set_map(map, key3, data3);
+    set_map(map, key4, data4);
+    printf("Get %s val : %s\n", key3, get_map(map, key3).payload);
+    del_map(map, key4);
     clean_up(map);
-    printf("=== End map testing ===");
+    printf("=== End map testing ===\n");
     return 0;
 }
--- self_define.h
+++ self_define.h
@@ -4,25 +4,31 @@
 #define DEV_NAME "simple_dev"
 #define MAP_SIZE 10
 
+/*
+Try a simple solution.
+*/
 // packet data
 struct packet_data
 {
     unsigned char payload[2048];
-    struct packet_data *next;
+    // struct packet_data *next;
 };
 
 // flow data
-struct flow_data
-{
-    unsigned char data[16];
-    struct packet_data *next;
-};
+// struct flow_data {
+//     unsigned char data[16];
+//     struct packet_data *head;
+// };
 
+/*
+Try a simple solution.
+*/
 // flow management key value pair
 struct kv
 {
     unsigned char key[16];
-    struct flow_data *val;
+    // struct flow_data *val;
+    struct packet_data val;
     struct kv *next;
 };
 
@@ -35,8 +41,8 @@ struct hmap
 
 // flow management map api interface
 struct hmap *map_init(unsigned int);
-int set_map(struct hmap *, char *, struct flow_data *);
-struct flow_data *get_map(struct hmap *, char *);
+int set_map(struct hmap *, char *, struct packet_data);
+struct packet_data get_map(struct hmap *, char *);
 void del_map(struct hmap *, char *);
 void clean_up(struct hmap *);
 
