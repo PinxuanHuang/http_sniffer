487dd58d9a0a6015ab0b13aa631fa35360e2b865
PinxuanHuang <hatedog77777@gmail.com>
2023-06-05 01:38:16 +0800

    Modify flow_table and flow_data, packet_data struct

--- self_define.h
+++ self_define.h
@@ -28,14 +28,15 @@ typedef struct pcaprec_hdr_s
 /* packet data storage structure */
 struct packet_data
 {
-    unsigned char payload[2048]; /* packet payload, including header */
-    struct packet_data *next;    /* next packet */
+    unsigned char payload[2048];  /* packet payload, including header */
+    unsigned int payload_len;     /* payload length */
+    struct packet_data *pkt_next; /* next packet */
 };
 
 /* flow data */
 struct flow_data
 {
-    struct packet_data *head;
+    struct packet_data *packet_info; /* all packets' info of the flow */
 };
 
 /* key structure */
@@ -49,27 +50,33 @@ struct flow_key
     unsigned char padding[3];
 };
 
-/* hashmap key-value pair structure */
-struct key_value
+/* hasflow_table key-value pair structure */
+struct flow_entry
 {
-    struct flow_key key; /* hashmap key */
-    struct flow_data value;
-    struct key_value *next; /* next key-value under the same bucket */
+    struct flow_key five_tuple_key; /* hasflow_table key */
+    struct flow_data flow_info;     /* packets' info */
+    struct flow_entry *flow_next;   /* next key-value under the same bucket */
 };
 
-/* hashmap structure */
-struct hmap
+/* hasflow_table structure */
+struct flow_table
 {
-    unsigned int size;      /* hashmap bucket size */
-    struct key_value **map; /* pointers to key-value pair pointer */
+    unsigned int size;           /* hasflow_table bucket size */
+    struct flow_entry **buckets; /* pointers to key-value pair pointer */
 };
 
-/* flow management map api interface */
-struct hmap *map_init(unsigned int);
-int set_map(struct hmap *, struct flow_key, struct packet_data *);
-struct flow_data get_map(struct hmap *, struct flow_key);
-void del_map(struct hmap *, struct flow_key);
-void clean_up(struct hmap *);
+/* Following is the flow management api interface definition */
+struct flow_table *flow_table_init(unsigned int); /* init a flow table */
+struct flow_entry *flow_table_get_bucket(struct flow_table *, struct flow_key);
+struct flow_entry *flow_table_flow_exist(struct flow_entry *, struct flow_key);
+int flow_table_add_flow(struct flow_table *, struct flow_key);               /* add a flow to the flow table */
+struct flow_data *flow_table_get_flow(struct flow_table *, struct flow_key); /* get a flow's data from the flow table */
+void flow_table_del_flow(struct flow_table *, struct flow_key);              /* delete a flow from the flow table */
+
+int flow_data_add_packet(struct flow_table *, struct flow_key key, struct packet_data *); /* add a packet to the flow */
+void flow_data_del_packets(struct flow_table *, struct flow_key);                         /* del all packets of the flow */
+
+void flow_table_clean_up(struct flow_table *); /* clean up all flows and packets from the flow table */
 
 /* ioctl cmd definition*/
 enum ops
